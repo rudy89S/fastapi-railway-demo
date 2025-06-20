5dae9969ba9acfc3457b2b619a5cdb1565a9cbc8
Rudy Sanchez <rodolfosanchez71@gmail.com>
2025-06-19 20:56:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI + Railway works!"}
+
+@app.on_event("startup")
+def connect_db():
+    try:
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        cursor = conn.cursor()
+        cursor.execute("SELECT current_database();")
+        db = cursor.fetchone()
+        print(f" Connected to database: {db[0]}")
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f" Database connection failed: {e}")
+
